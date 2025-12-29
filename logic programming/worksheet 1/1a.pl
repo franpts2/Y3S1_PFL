ce8ea9ce2c9d74cccd06403c01e9a2a986b75c38
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-12-29 11:16:10 +0000

    Added ex 1b queries

--- logic programming/worksheet 1/1a.pl
+++ logic programming/worksheet 1/1a.pl
@@ -64,3 +64,69 @@ parent(cameron,rexford).
 % pameron and bo are parents of calhoun
 parent(pameron,calhoun).
 parent(bo,calhoun).
+
+/* 1b)
+i. Is Haley a female?
+| ?- female(haley).
+yes
+
+ii. Is Gil a male?
+| ?- male(gil).  
+no
+
+iii. Is Frank a parent of Phil?
+| ?- parent(frank,phil).
+yes
+
+iv. Who are Claire’s parents?
+| ?- parent(X,claire).
+X = dede ? ;
+X = jay ? ;
+no
+
+v. Who are Gloria’s children?
+| ?- parent(gloria,X).
+X = joe ? ;
+X = manny ? ;
+no
+
+vi. Who are Jay’s grandchildren?
+| ?- parent(jay,Child), parent(Child, Grandchild).
+Child = claire,
+Grandchild = haley ? ;
+Child = claire,
+Grandchild = alex ? ;
+Child = claire,
+Grandchild = luke ? ;
+Child = mitchell,
+Grandchild = lily ? ;
+Child = mitchell,
+Grandchild = rexford ? ;
+no
+
+vii. Who are Lily’s grandparents?
+| ?- parent(Grandparent, Parent), parent(Parent,lily).
+Grandparent = dede,
+Parent = mitchell ? ;
+Grandparent = jay,
+Parent = mitchell ? ;
+Grandparent = barb,
+Parent = cameron ? ;
+Grandparent = merle,
+Parent = cameron ? ;
+no
+
+viii. Does Alex have children?
+| ?- parent(alex,X).
+no
+
+ix. Who are Luke’s siblings (child of both Luke’s parents)?
+| ?- parent(P1, luke), parent(P2, luke), P1 @< P2, parent(P1, Sibling), parent(P2, Sibling), Sibling \= luke.
+P1 = claire,
+P2 = phil,
+Sibling = haley ? ;
+P1 = claire,
+P2 = phil,
+Sibling = alex ? ;
+no
+*/
---
+++ logic programming/worksheet 1/README.md
@@ -0,0 +1,3 @@
+The exercises I had more difficulty solving were:
+- 1 b) vi. 
+- 1 b) ix.
