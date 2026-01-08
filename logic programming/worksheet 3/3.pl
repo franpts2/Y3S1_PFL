0a4ee64989774265f5684ef59262ba80148fbbfb
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-08 18:33:18 +0000

    Added ex 3a and 3b

---
+++ logic programming/worksheet 3/3.pl
@@ -0,0 +1,16 @@
+% a)
+invert(List1,List2):-
+	invert_aux(List1,[],List2).
+
+invert_aux([],Acc,Acc).
+
+invert_aux([H | T],Acc,List2):-
+	invert_aux(T,[H|Acc],List2).
+
+% b)
+del_one(Elem,[Elem|T],T). % Head = Elem
+
+del_one(Elem,[H|T],[H|Rest]):-
+	Elem \= H,
+	del_one(Elem,T,Rest).
+
--- logic programming/worksheet 3/README.md
+++ logic programming/worksheet 3/README.md
@@ -1,3 +1,5 @@
 The exercises I had more difficulty solving were:
 - 2 a)
 - 2 e)
+- 3 a)
+- 3 b)
