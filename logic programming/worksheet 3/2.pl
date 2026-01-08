7b9954cae8516c217cbb909094f078b16bdd4424
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-08 17:08:52 +0000

    Added ex 2b

--- logic programming/worksheet 3/2.pl
+++ logic programming/worksheet 3/2.pl
@@ -7,4 +7,12 @@ list_size([_|T],Size):-
 
 % b)
 list_sum(List,Sum):-
-	
+	list_sum_aux(List,0,Sum).
+
+list_sum_aux([],Acc,Acc).
+
+list_sum_aux([H|T],Acc,Sum):-
+	Acc1 is Acc+H,
+	list_sum_aux(T,Acc1,Sum).
+
+% c)
