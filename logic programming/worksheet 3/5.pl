7af3174a1da394b503c1c8a0b69d4c86c139383c
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-09 19:33:39 +0000

    Added ex 5b

--- logic programming/worksheet 3/5.pl
+++ logic programming/worksheet 3/5.pl
@@ -8,3 +8,14 @@ list_to_aux(N,Acc,List):-
 	N > 0,
 	N1 is N - 1,
 	list_to_aux(N1,[N|Acc],List).
+
+% b)
+list_from_to(Inf,Sup,List):-
+	list_from_to_aux(Inf,Sup,[Sup],List).
+
+list_from_to_aux(Sup,Sup,Acc,Acc).
+
+list_from_to_aux(Inf,Sup,Acc,List):-
+	Sup > Inf,
+	Sup1 is Sup-1,
+	list_from_to_aux(Inf,Sup1,[Sup1|Acc],List).
