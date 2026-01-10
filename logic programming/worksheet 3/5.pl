071243a0343a0d4db805b206430db68acf2c72fc
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-10 10:13:23 +0000

    Added ex 5d

--- logic programming/worksheet 3/5.pl
+++ logic programming/worksheet 3/5.pl
@@ -26,5 +26,35 @@ list_from_to_step(Inf,Sup,_,[]):-
 
 list_from_to_step(Inf,Sup,Step,[Inf|T]):-
     Inf =< Sup,
-    Inf1 is Inf + Step;
+    Inf1 is Inf + Step,
     list_from_to_step(Inf1,Sup,Step,T).
+
+% d) 
+% 2 \= ways to implement: list_from_to_mod can use both but list_from_to_step_mod can only use its
+my_reverse(List1,List2):-
+    my_reverse_aux(List1,[],List2).
+
+my_reverse_aux([],Acc,Acc).
+
+my_reverse_aux([H|T],Acc,List2):-
+    my_reverse_aux(T,[H|Acc],List2).
+
+list_from_to_mod(Inf,Sup,List):-
+    Inf > Sup, !,                    % '!' stops it from trying the next rule
+    list_from_to(Sup,Inf,Temp),
+    my_reverse(Temp,List).
+
+list_from_to_mod(Inf,Sup,List):-
+    list_from_to(Inf,Sup,List).
+
+
+list_from_to_step_mod(Inf,Sup,Step,[Inf|T]):-
+    Inf > Sup, !,
+    Inf1 is Inf - Step,
+    list_from_to_step_mod(Inf1,Sup,Step,T).
+
+list_from_to_step_mod(Inf,Sup,_,[]):-
+    Inf < Sup, !.
+
+list_from_to_step_mod(Inf,Sup,Step,List):-
+    list_from_to_step(Inf,Sup,Step,List).
