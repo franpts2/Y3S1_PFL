d8bf76bc57eaadc800b271acfc404ef792b96d0c
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-11 21:38:18 +0000

    Added ex 3a

---
+++ logic programming/worksheet 5/3.pl
@@ -0,0 +1,17 @@
+% class(Course,ClassType,DayOfWeek,Time,Duration)
+class(pfl, t, '2 Tue', 15, 2).
+class(pfl, tp, '2 Tue', 10.5, 2).
+class(lbaw, t, '3 Wed', 10.5, 2).
+class(lbaw, tp, '3 Wed', 8.5, 2).
+class(ipc, t, '4 Thu', 14.5, 1.5).
+class(ipc, tp, '4 Thu', 16, 1.5).
+class(fsi, t, '1 Mon', 10.5, 2).
+class(fsi, tp, '5 Fri', 8.5, 2).
+class(rc, t, '5 Fri', 10.5, 2).
+class(rc, tp, '1 Mon', 8.5, 2).
+
+% a)
+same_day(Course1,Course2):-
+	class(Course1,_,Day,_,_),
+	class(Course2,_,Day,_,_),
+	Course1 \= Course2.
