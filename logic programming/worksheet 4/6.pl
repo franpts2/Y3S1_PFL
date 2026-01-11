518757bed6935123c7440d4cff8e7e75a4604f16
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-11 12:12:25 +0000

    Added ex 6b

--- logic programming/worksheet 4/6.pl
+++ logic programming/worksheet 4/6.pl
@@ -15,3 +15,49 @@ print_full_list_aux([H|T]):-
 	write(H),
 	write(', '),
 	print_full_list_aux(T).
+
+% b)
+print_list(L):-
+	length(L,Len),
+	Len < 12,
+	print_full_list(L), !.
+
+print_list(L):-
+	write('['),
+	print_first_three(L),
+	write(',..., '),
+	print_middle_three(L),
+	write(',..., '),
+	print_last_three(L),
+    write(']'), nl.
+
+print_first_three(L):-
+    length(Prefix,3),
+    append(Prefix,_,L),
+
+    print_3elements(Prefix).
+
+print_middle_three(L):-
+    length(L,TotalLen),
+    Skip is (TotalLen - 2) // 2,
+
+    length(Prefix,Skip),
+    append(Prefix,MiddleAndSuffix,L),
+
+    append(MiddleThree,_,MiddleAndSuffix),
+    length(MiddleThree,3),
+
+    print_3elements(MiddleThree).
+
+print_last_three(L):-
+    length(L,TotalLen),
+    Skip is TotalLen - 3,
+
+    length(Suffix,3),
+    length(SkipPrefix,Skip),
+    append(SkipPrefix,Suffix,L),
+
+    print_3elements(Suffix).
+
+print_3elements([A, B, C]) :-
+    format('~w, ~w, ~w', [A, B, C]).
--- logic programming/worksheet 4/README.md
+++ logic programming/worksheet 4/README.md
@@ -2,3 +2,4 @@ The exercises I had more difficulty solving were:
 - 2
 - 5 d)
 - 5 e)
+- 6 b)
