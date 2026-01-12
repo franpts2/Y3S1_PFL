3bd9a2b945188690a22021bf68373caab3d6fd1a
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-12 19:17:53 +0000

    Added ex 3b

--- logic programming/worksheet 6/3.pl
+++ logic programming/worksheet 6/3.pl
@@ -10,3 +10,11 @@ tree_size(node(_,Left,Right), Size):-
     tree_size(Left,SL),
     tree_size(Right,SR),
     Size is 1 + SL + SR.
+
+% b)
+tree_map(_,null,null).
+
+tree_map(Pred,node(V1,L1,R1),node(V2,L2,R2)):-
+    call(Pred,V1,V2), % Pred(V1,V2)
+    tree_map(Pred,L1,L2),
+    tree_map(Pred,R1,R2).
--- logic programming/worksheet 6/README.md
+++ logic programming/worksheet 6/README.md
@@ -2,4 +2,4 @@ The exercises I had more difficulty solving were:
 - 1 a)
 - 1 b)
 - 2
-- 3 a)
+- 3 b)
