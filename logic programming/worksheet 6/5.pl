1ddcedf70d9dbba8b83061a2c7e74e74f15f6beb
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-12 22:14:42 +0000

    Added ex 5b

--- logic programming/worksheet 6/5.pl
+++ logic programming/worksheet 6/5.pl
@@ -9,4 +9,13 @@ class  pfl tuesdays   15
   /
   t
 
+b) tp class of pfl on mondays at 10:30.
+
+		   on 
+        /      \
+     of          at 
+    /  \      /      \
+class  pfl  mondays   : 
+  /                /     \
+  tp              10      30
 */
