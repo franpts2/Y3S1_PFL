b315756cd6a41a67dee5610470bb616b76bd0e75
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-12 17:58:47 +0000

    Added ex 1b

--- logic programming/worksheet 6/1.pl
+++ logic programming/worksheet 6/1.pl
@@ -9,3 +9,12 @@ map(Pred,[H1|T1],[H2|T2]):-
 	call(G),
 
 	map(Pred,T1,T2).
+
+% b)
+fold(_,FinalValue,[],FinalValue).
+
+fold(Pred,StartValue,[H|T],FinalValue):- % StartValue behaves as Acc
+	call(Pred,StartValue,H,NewAcc), % Pred(Acc,H,NewAcc)
+	fold(Pred,NewAcc,T,FinalValue).
+
+sum(A, B, S):- S is A+B.
--- logic programming/worksheet 6/README.md
+++ logic programming/worksheet 6/README.md
@@ -1,2 +1,3 @@
 The exercises I had more difficulty solving were:
 - 1 a)
+- 1 b)
