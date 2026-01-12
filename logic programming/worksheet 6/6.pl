a19a1da20409f429e8ad88c179e2875791113d41
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-01-12 22:26:34 +0000

    Added ex 6a

---
+++ logic programming/worksheet 6/6.pl
@@ -0,0 +1,16 @@
+% a)
+:- op(550, fx, flight).
+:- op(580, xfx, from).
+:- op(570, xfx, to).
+:- op(570, xfx, at).
+:- op(550, xfy, :).
+
+/*
+		  to
+        /      \
+     from        at
+    /   \     /      \
+flight porto  lisbon  :
+   \               /     \
+tp1949            16      15
+*/
